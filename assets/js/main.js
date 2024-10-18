f6da1fc5aa2d317d8a3769da46b86359eb817696
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-18 16:01:55 +0200

    loop for

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ console.log('it works');
 const rowPhotoEl = document.querySelector('.row_photo');
 
 
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 .then(response =>{
 
     //richiesta
@@ -11,9 +11,30 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     console.log(response);
 
     const postsPhoto = response.data;
-    
+
     let postElements = ''
-    postsPhoto.forEach(post =>{
+
+    for(let i= 0; i<postsPhoto.lenght; i++){
+
+        const post = postsPhoto[i];
+
+        const{title, url, thumbnailUrl} = post
+        const markup =  ` 
+                    <div class="col-sm-12 col-md-3 col-lg-1 col_photo hover_photo">
+                        <span class="dot"></span>
+                        <div class="card photo_space">
+                            ${thumbnailUrl}
+                        </div>
+                        <div>
+                        <h2 class="description ">${title}</h2> 
+                        </div>       
+                     </div> `
+
+
+                postElements+= markup
+    }
+    /*postsPhoto.forEach(post =>{
+
         const {title, url, thumbnailUrl} = post
 
         const markup =  ` 
@@ -30,10 +51,11 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
                 postElements+= markup
 
-        }
+        }*/
     
         rowPhotoEl.innerHTML= postElements
-)
+       
+        
 })
 
 
