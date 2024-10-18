c762b94ffae9f60426a67bc87c4cc5aeeacad621
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-18 15:51:51 +0200

    add axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
 console.log('it works');
+
+const rowPhotoEl = document.querySelector('.row_photo');
+
+
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+.then(response =>{
+
+    //richiesta
+
+    console.log(response);
+
+    const postsPhoto = response.data;
+    
+    let postElements = ''
+    postsPhoto.forEach(post =>{
+        const {title, url, thumbnailUrl} = post
+
+        const markup =  ` 
+                    <div class="col-sm-12 col-md-3 col-lg-1 col_photo hover_photo">
+                        <span class="dot"></span>
+                        <div class="card photo_space">
+                            ${thumbnailUrl}
+                        </div>
+                        <div>
+                        <h2 class="description ">${title}</h2> 
+                        </div>       
+                     </div> `
+
+
+                postElements+= markup
+
+        }
+    
+        rowPhotoEl.innerHTML= postElements
+)
+})
+
+
+
+
+
