71b15d817b23aab09e22c67efbc44b1eb7b2c33d
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-21 17:07:27 +0200

    fix function

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ const rowEl = document.querySelector('.row')
 //const overLayEl = document.getElementById("overlay")
 
 //const img_fullEL = document.getElementById('img_full')
-const overLayimgEL= document.getElementById("img_full[i]");
-console.log(overLayimgEL);
+const overLayimgEL= document.querySelectorAll(".overlay_img");
+//console.log(overLayimgEL);
 
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
@@ -30,7 +30,7 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
                     <div class="col-3 col_photo hover_photo">
                         <span class="dot"></span>
                         <div class="card photo_space">
-                        <img src="${url}" onclick="on()" class="overlay_img" alt="">
+                        <img src="${url}" onclick="on()" class="" alt="">
                         </div>
                         <div>
                         <h2 class="description ">${title}</h2> 
@@ -59,13 +59,23 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 
     overlayEl.innerHTML = postOverLay;
 
+   
+    const overLayimgEL= document.querySelectorAll(".overlay_img");
+
+    overLayimgEL.forEach((image)=> image.addEventListener('click', function on() {
+            overlayEl.style.display = "block";  // Mostra l'overlay quando si clicca su un'immagine
+        }));
+    /*overLayimgEL.addEventListener('click', function on(){
+        overLayElnew.style.display = "block";})*/
+
     
 })
-    
 
+    
+/*
 
         //const postOver = postsPhoto[i];
-/*
+
         const{ url} = postOver
         const markup =  `                     
                         <img src="${url}" onclick="on()" class="overlay_img" alt="">
@@ -100,85 +110,4 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 });
 
 
-   
-
-    
-     /*const img = postsPhoto.forEach((post) =>{ 
-        post.addEventListener('click'=>function On( imgUrl , overlay){
-            const overLayEl = document.getElementById("overlay")+post.url
-        })
-        //console.log (post.url)
-        }         )*/
-    
-
- 
-    
-    /*
-    const overLayimgEL= document.getElementById("img_full[i]");
-    console.log(overLayimgEL);
-    
-  /*overLayEl.addEventListener('click', function On() {
-
-    overLayEl.style.display = "block";
-
-    //overLayEl.innerHTML = `${url}`--> sbagliata l'ovrlay deve essere mostrata insieme all'immagine
-}*/
-
-    //overLayEl.style.display = "none";
-
-    //const overLayEl = document.getElementById("overlay").style.display = "none"; --> inserito in CSS
-
-    
-  
-/*
-        
-    
-})
-
-
-function on(imgUrl) {
-    const overLayEl = document.getElementById("overlay") //.style.display = "block";
-    
-
-    overLayimgEL.src = imgUrl;
-    overLayEl.style.display = "block";
-    //.style.display = "block";
-
-  }
-  function off() {
-    const overlayEl = document.getElementById("overlay");
-    overlayEl.style.display = "none";  // Nascondi l'overlay
-}
-  /*function off(url) {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("img_full").style.display = "none";
-  }*/
-
-
-/*img_fullEL.addEventListener('click', function() {
-    if (document.getElementById("overlay").style.display = "none" ) {
-        document.getElementById("overlay").style.display = "block";
-    } 
-    else if(document.getElementById("overlay").style.display = "block") {
-        
-    }
-})*/
-/*function on() {
- 
-
-    document.getElementById("overlay")
-    .style.display = "block";
-    }*/
-  
-
-   
-
-    
-
-//document.addEventListener("click", 
-   
-
-
-
-
-
+ */
