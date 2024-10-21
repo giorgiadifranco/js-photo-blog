9807d80ceccc5a81fe7badee89179bc4e94a2c68
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-21 11:50:58 +0200

    add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ console.log('it works');
 
 const rowPhotoEl = document.querySelector('.row_photo');
 const rowEl = document.querySelector('.row')
-
+//const overlayEl = document.querySelector('.overlay')
+const overLayEl = document.getElementById("overlay")
 
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
@@ -25,7 +26,7 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
                     <div class="col-3 col_photo hover_photo">
                         <span class="dot"></span>
                         <div class="card photo_space">
-                        <img src="${url}" onclick="on()" alt="">
+                        <img src="${url}" onclick="on()" class="img_full" alt="">
                         </div>
                         <div>
                         <h2 class="description ">${title}</h2> 
@@ -39,22 +40,38 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     
     rowPhotoEl.innerHTML= postElements
 
+    
+  overLayEl.addEventListener('click', function On() {
+
+    overLayEl.style.display = "block";
+
+    //overLayEl.innerHTML = `${url}`--> sbagliata l'ovrlay deve essere mostrata insieme all'immagine
+}
+
+    //overLayEl.style.display = "none";
+
+    //const overLayEl = document.getElementById("overlay").style.display = "none"; --> inserito in CSS
+
+    
+  )
+
         
-       
-        
+    
 })
+/*function on() {
+ 
+
+    document.getElementById("overlay")
+    .style.display = "block";
+    }*/
+  
+
+   
+
+    
 
 //document.addEventListener("click", 
-    function on() {
-
-
-    document.getElementById("overlay").style.display = "block";
-    }
-  
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+   
 
 
 
